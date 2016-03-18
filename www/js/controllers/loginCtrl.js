e66b79d424c433ed2dcd0e688d52945207663bcd
Maxwell Zirbel <maxwellzirbel@Maxwells-MacBook-Pro.local>
2016-03-18 12:18:45 -0500

    picture takes and attaches to email

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -3,26 +3,29 @@
  */
 angular.module('starter.controller.login', [])
 
-  .controller('LoginCtrl', function($scope, $state, $rootScope, $firebaseAuth) {
+  .controller('LoginCtrl', function($scope, $state, $rootScope) {
     $scope.user = {
       email: 'test2@test.com',
       password: 'test'
     };
 
   $scope.login = function(){
-    var ref = new Firebase("https://sendphoto.firebaseio.com");
 
-    var auth = $firebaseAuth(ref);
+    $state.go('picture');  
+      
+    // var ref = new Firebase("https://sendphoto.firebaseio.com");
 
-    auth.$authWithPassword({
-      email: $scope.user.email,
-      password: $scope.user.password
-    }).then(function(authData) {
-      console.log("Logged in as:", authData);
-      $state.go('picture')
-    }).catch(function(error) {
-      console.error("Authentication failed:", error);
-    });
+    // var auth = $firebaseAuth(ref);
+
+    // auth.$authWithPassword({
+    //   email: $scope.user.email,
+    //   password: $scope.user.password
+    // }).then(function(authData) {
+    //   console.log("Logged in as:", authData);
+    //   $state.go('picture')
+    // }).catch(function(error) {
+    //   console.error("Authentication failed:", error);
+    // });
   }
 
   });
--- www/js/controllers/pictureCtrl.js
+++ www/js/controllers/pictureCtrl.js
@@ -3,30 +3,57 @@
  */
 angular.module('starter.controller.picture', [])
 
-  .controller('PictureCtrl', function($scope, $state, $rootScope, $cordovaEmailComposer) {
+  .controller('PictureCtrl', function($scope, $state, $rootScope, $cordovaEmailComposer, $cordovaCamera, $ionicPlatform) {
       $scope.takePicture = function () {
-        try{
-          navigator.camera.getPicture(onSuccess, onFail, { quality: 25,
-            destinationType: Camera.DestinationType.DATA_URL
-          });
+     
+        alert("here");     
 
-          function onSuccess(imageData) {
-            var image = document.getElementById('myImage');
-            image.src = "data:image/jpeg;base64," + imageData;
-          }
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 100,
+            targetHeight: 100,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false,
+            correctOrientation:true
+        };
 
-          function onFail(message) {
-            alert('Failed because: ' + message);
-          }
-        }catch(e){
-          console.log(e);
-          console.log(e.stack);
-          console.log(e.line);
-        }
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            var image = "data:image/jpeg;base64," + imageData;
+            $scope.email(imageData);
+        }, function(err) {
+            // error
+        });
+     
+     
+          
+    //    alert("called");   
+    //     try{
+    //       navigator.camera.getPicture(onSuccess, onFail, { quality: 25,
+    //         destinationType: Camera.DestinationType.DATA_URL
+    //       });
+
+    //       function onSuccess(imageData) {
+    //         var image = document.getElementById('myImage');
+    //         image.src = "data:image/jpeg;base64," + imageData;
+    //       }
+
+    //       function onFail(message) {
+    //         alert('Failed because: ' + message);
+    //       }
+    //     }catch(e){
+    //       console.log(e);
+    //       console.log(e.stack);
+    //       console.log(e.line);
+    //     }
 
       }
 
-    $scope.email = function(){
+
+    $scope.email = function(image){
       try{
         $cordovaEmailComposer.isAvailable().then(function() {
           alert('yes')
@@ -38,6 +65,9 @@ angular.module('starter.controller.picture', [])
           to: 'baumgs86@uwosh.edu',
           cc: 'sean.baumgartner14@gmail.com',
           bcc: [],
+          attachments: [
+              'base64:recept.png//' + image
+          ],
           //attachments: [
           //  'file://img/logo.png',
           //  'res://icon.png',
@@ -45,7 +75,7 @@ angular.module('starter.controller.picture', [])
           //  'file://README.pdf'
           //],
           subject: 'Receipt',
-          body: 'Here is the receipt!',
+          body: 'Here is the receipt',
           isHtml: true
         };
 
